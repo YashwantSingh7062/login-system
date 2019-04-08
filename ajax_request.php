48349dc5453781630206a485a80c924ca28076ae
Yashwant Singh <33049135+YashwantSingh7062@users.noreply.github.com>
2019-04-08 01:05:35 -0700

    Add files via upload
    Committed additional files for this project.

---
+++ ajax_request.php
@@ -0,0 +1,35 @@
+<?php
+if(isset($_POST['action'])){
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Validating form data
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if($_POST['action'] == "form-validation"){
+        // This validation can also be made by validating data from the database.
+        $valid_username = "yashwant";
+        $valid_password = "12345";
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        if($username == $valid_username && $password == $valid_password){
+            echo "<span class='text-success'>You are logged in.</span>";
+        }
+        else{
+            echo "<span class='text-danger'>Username or Password is incorrect.</span>";
+        }
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Checking if username is available or not
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    else if($_POST['action'] == 'signup_username_suggest'){
+        if($_POST['query'] == "yashwant"){
+            echo "<span class='text-danger'>Username not available</span>";
+        }
+        else{
+            echo "<span class='text-success'>Username available</span>";
+        }
+    }
+}
+else{
+    header("location:index.php");
+}
+?>
